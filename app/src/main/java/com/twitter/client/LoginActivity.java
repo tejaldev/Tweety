b0e009816a3875b3e80b4ab1e2fc3a5df6807c63
Tejal Par <parulekar.t@gmail.com>
2017-09-29 13:26:07 -0700

    use codepath Oauth, rest client, login activity

---
+++ app/src/main/java/com/twitter/client/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.twitter.client;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.codepath.oauth.OAuthLoginActionBarActivity;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.twitter.client.network.TweetClient;
+import com.twitter.client.network.response.models.User;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class LoginActivity extends OAuthLoginActionBarActivity<TweetClient> {
+    private static String TAG = LoginActivity.class.getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+
+	// Inflate the menu; this adds items to the action bar if it is present.
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	// OAuth authenticated successfully, launch primary authenticated activity
+	// i.e Display application "homepage"
+	@Override
+	public void onLoginSuccess() {
+        fetchLoggedInUserDetails();
+		Intent i = new Intent(this, TweetListActivity.class);
+		startActivity(i);
+	}
+
+	// OAuth authentication flow failed, handle the error
+	// i.e Display an error dialog or toast
+	@Override
+	public void onLoginFailure(Exception e) {
+		Toast.makeText(this, "Login failed." + e.getMessage(), Toast.LENGTH_LONG).show();
+		e.printStackTrace();
+	}
+
+	// Click handler method for the button used to start OAuth flow
+	// Uses the client to initiate OAuth authorization
+	// This should be tied to a button used to login
+	public void loginToRest(View view) {
+		getClient().connect();
+	}
+
+    private void fetchLoggedInUserDetails() {
+        TweetApplication.getTweetRestClient().getUserDetails(null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d(TAG, "fetchLoggedInUserDetails:: Response received successfully.");
+                TweetApplication.setLoggedInUser(User.parseUserInfoFromJson(response));
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d(TAG, "fetchLoggedInUserDetails:: Response received successfully.");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e(TAG, "fetchLoggedInUserDetails:: Failed to receive api response: " + responseString, throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.e(TAG, "fetchLoggedInUserDetails:: Failed to receive api response: " + errorResponse, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/twitter/client/network/TweetClient.java
@@ -0,0 +1,91 @@
+package com.twitter.client.network;
+
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.core.builder.api.BaseApi;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.twitter.client.R;
+
+/*
+ * 
+ * This is the object responsible for communicating with a REST API. 
+ * Specify the constants below to change the API being communicated with.
+ * See a full list of supported API classes: 
+ *   https://github.com/scribejava/scribejava/tree/master/scribejava-apis/src/main/java/com/github/scribejava/apis
+ * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
+ * Add methods for each relevant endpoint in the API.
+ * 
+ * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
+ * 
+ */
+public class TweetClient extends OAuthBaseClient {
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
+	public static final String REST_CONSUMER_KEY = "z8rOo8sEdqmGCCyD4LsYKG7Wh";       // Change this
+	public static final String REST_CONSUMER_SECRET = "uX5JoKg4YKdiqqs4NoomabnCIWIkG0TVMH7TtsMBnyOwWOxjjG"; // Change this
+
+	public static final String HOME_TIMELINE_TWEETS_RELATIVE_URL = "statuses/home_timeline.json";
+
+	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
+	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
+
+	// See https://developer.chrome.com/multidevice/android/intents
+	public static final String REST_CALLBACK_URL_TEMPLATE = "intent://%s#Intent;action=android.intent.action.VIEW;scheme=%s;package=%s;S.browser_fallback_url=%s;end";
+
+	public TweetClient(Context context) {
+		super(context, REST_API_INSTANCE,
+				REST_URL,
+				REST_CONSUMER_KEY,
+				REST_CONSUMER_SECRET,
+				String.format(REST_CALLBACK_URL_TEMPLATE, context.getString(R.string.intent_host),
+						context.getString(R.string.intent_scheme), context.getPackageName(), FALLBACK_URL));
+	}
+	// CHANGE THIS
+	// DEFINE METHODS for different API endpoints here
+	public void getInterestingnessList(AsyncHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("format", "json");
+		client.get(apiUrl, params, handler);
+	}
+
+	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
+	 * 	  i.e getApiUrl("statuses/home_timeline.json");
+	 * 2. Define the parameters to pass to the request (query or body)
+	 *    i.e RequestParams params = new RequestParams("foo", "bar");
+	 * 3. Define the request method and make a call to the client
+	 *    i.e client.get(apiUrl, params, handler);
+	 *    i.e client.post(apiUrl, params, handler);
+	 */
+	public void getHomeTimelineTweets(RequestParams params, AsyncHttpResponseHandler responseHandler) {
+		String apiUrl = getApiUrl(HOME_TIMELINE_TWEETS_RELATIVE_URL);
+		// Can specify query string params directly or through RequestParams.
+		RequestParams requestParams = buildRequestParams(params);
+		client.get(apiUrl, requestParams, responseHandler);
+	}
+
+	public void getUserDetails(RequestParams params, AsyncHttpResponseHandler responseHandler) {
+		String apiUrl = getApiUrl("account/verify_credentials.json");
+		RequestParams requestParams = buildRequestParams(params);
+		client.get(apiUrl, requestParams, responseHandler);
+	}
+
+	public void postNewTweet(RequestParams params, AsyncHttpResponseHandler responseHandler) {
+		String apiUrl = getApiUrl("statuses/update.json");
+		RequestParams requestParams = buildRequestParams(params);
+		client.post(apiUrl, requestParams, responseHandler);
+	}
+
+	private RequestParams buildRequestParams(RequestParams params) {
+		if (params == null) {
+			params = new RequestParams();
+		}
+		params.put("format", "json");
+
+		return params;
+	}
+}
